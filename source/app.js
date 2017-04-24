f52cb6dfe6864053dedbea705fd8a0782050c046
Jorge Calás Lozano <calas@qvitta.net>
2017-04-24 19:38:05 +0200

    First React Element

---
+++ source/app.js
@@ -0,0 +1,6 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var element = React.createElement('h1')
+
+ReactDOM.render(element, document.getElementById('react-application'))
