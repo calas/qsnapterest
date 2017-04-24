a75b54a3c035a9df4762929e6d08369e2a22f2b3
Jorge Calás Lozano <calas@qvitta.net>
2017-04-24 19:44:45 +0200

    React Nodes containing other React Nodes.
    Using React fragments, texts and elements to build complex React nodes.

--- build/qsnapterest.js
+++ build/qsnapterest.js
@@ -20771,8 +20771,14 @@ module.exports = require('./lib/React');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var reactElement = React.createElement('h1', { className: 'header', key: 'header' }, 'This is React.js');
+var h1 = React.createElement('h1', { className: 'header', key: 'header' }, 'This is React.js');
 
-ReactDOM.render(reactElement, document.getElementById('react-application'));
+var p = React.createElement('p', { className: 'content', key: 'content' }, "And that's how it works.");
+
+var reactFragment = [h1, p];
+
+var section = React.createElement('section', { className: 'container' }, reactFragment);
+
+ReactDOM.render(section, document.getElementById('react-application'));
 
 },{"react":182,"react-dom":29}]},{},[183]);
