349f715be13037782e14cb1e9e670226e13c67e4
Jorge Calás Lozano <calas@qvitta.net>
2017-04-25 14:22:20 +0200

    Update StreamTweet component to fetch twitter stream

--- source/components/Stream.react.js
+++ source/components/Stream.react.js
@@ -1,8 +1,41 @@
 const React = require('react')
+const SnapkiteStreamClient = require('snapkite-stream-client')
+const StreamTweet = require('./StreamTweet.react')
+const Header = require('./Header.react')
 
 class Stream extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { tweet: null }
+    this.handleNewTweet = this.handleNewTweet.bind(this)
+  }
+
+  componentDidMount () {
+    SnapkiteStreamClient.initializeStream(this.handleNewTweet)
+  }
+
+  componentWillUnmount () {
+    SnapkiteStreamClient.destroyStream()
+  }
+
+  handleNewTweet (tweet) {
+    this.setState({ tweet: tweet })
+  }
+
   render () {
-    return null
+    let tweet = this.state.tweet
+
+    if (tweet) {
+      return (
+        <StreamTweet
+          tweet={tweet}
+          onAddTweetToCollection={this.props.onAddTweetToCollection} />
+      )
+    }
+
+    return (
+      <Header text='Waiting for public photos from Twitter...' />
+    )
   }
 }
 
