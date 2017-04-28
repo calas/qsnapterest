a70899fb7baccc8302ffb8c2fd0301fb13a92e77
Jorge Calás Lozano <calas@qvitta.net>
2017-04-28 02:37:35 +0200

    Refactor Stream component to use TweetStore

--- source/components/Stream.react.js
+++ source/components/Stream.react.js
@@ -1,36 +1,38 @@
 const React = require('react')
-const SnapkiteStreamClient = require('snapkite-stream-client')
 const StreamTweet = require('./StreamTweet.react')
 const Header = require('./Header.react')
+const TweetStore = require('../stores/TweetStore')
 
 class Stream extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { tweet: null }
-    this.handleNewTweet = this.handleNewTweet.bind(this)
+
+    this.state = {
+      tweet: TweetStore.getTweet()
+    }
+
+    this.onTweetChange = this.onTweetChange.bind(this)
   }
 
   componentDidMount () {
-    SnapkiteStreamClient.initializeStream(this.handleNewTweet)
+    TweetStore.addChangeListener(this.onTweetChange)
   }
 
   componentWillUnmount () {
-    SnapkiteStreamClient.destroyStream()
+    TweetStore.removeChangeListener(this.onTweetChange)
   }
 
-  handleNewTweet (tweet) {
-    this.setState({ tweet: tweet })
+  onTweetChange () {
+    this.setState({
+      tweet: TweetStore.getTweet()
+    })
   }
 
   render () {
     let tweet = this.state.tweet
 
     if (tweet) {
-      return (
-        <StreamTweet
-          tweet={tweet}
-          onAddTweetToCollection={this.props.onAddTweetToCollection} />
-      )
+      return <StreamTweet tweet={tweet} />
     }
 
     return (
