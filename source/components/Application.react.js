3e95dcb2fcc93c4818856a4839c7771fde6688b1
Jorge Calás Lozano <calas@qvitta.net>
2017-04-28 11:38:32 +0200

    Refactor Application component

--- source/components/Application.react.js
+++ source/components/Application.react.js
@@ -3,56 +3,15 @@ const Stream = require('./Stream.react')
 const Collection = require('./Collection.react')
 
 class Application extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      collectionTweets: {}
-    }
-
-    this.addTweetToCollection = this.addTweetToCollection.bind(this)
-    this.removeTweetFromCollection = this.removeTweetFromCollection.bind(this)
-    this.removeAllTweetsFromCollection = this.removeAllTweetsFromCollection.bind(this)
-  }
-
-  addTweetToCollection (tweet) {
-    this.setState(prevState => {
-      let collectionTweets = prevState.collectionTweets
-
-      collectionTweets[tweet.id] = tweet
-
-      return { collectionTweets: collectionTweets }
-    })
-  }
-
-  removeTweetFromCollection (tweet) {
-    this.setState(prevState => {
-      let collectionTweets = prevState.collectionTweets
-
-      delete collectionTweets[tweet.id]
-
-      return { collectionTweets: collectionTweets }
-    })
-  }
-
-  removeAllTweetsFromCollection () {
-    this.setState(prevState => {
-      return { collectionTweets: {} }
-    })
-  }
-
   render () {
     return (
       <div className='container-fluid'>
         <div className='row'>
           <div className='col-md-4 text-center'>
-            <Stream onAddTweetToCollection={this.addTweetToCollection} />
+            <Stream />
           </div>
           <div className='col-md-8'>
-            <Collection
-              tweets={this.state.collectionTweets}
-              onRemoveTweetFromCollection={this.removeTweetFromCollection}
-              onRemoveAllTweetsFromCollection={this.removeAllTweetsFromCollection} />
+            <Collection />
           </div>
         </div>
       </div>
